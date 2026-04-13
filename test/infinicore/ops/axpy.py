36372c0add38becd1ad03f605f7f84508939e79c
xuzhengzhong <xuzhzhong@163.com>
2026-04-13 12:17:42 +0000

    Remove `float16` and `bfloat16` support for `axpy`

--- test/infinicore/ops/axpy.py
+++ test/infinicore/ops/axpy.py
@@ -24,13 +24,11 @@ _TEST_CASES_DATA = [
 ]
 
 _TOLERANCE_MAP = {
-    infinicore.float16: {"atol": 1e-3, "rtol": 1e-2},
     infinicore.float32: {"atol": 1e-5, "rtol": 1e-4},
     infinicore.float64: {"atol": 1e-7, "rtol": 1e-6},
 }
 
 _TENSOR_DTYPES = [
-    # infinicore.float16,
     infinicore.float32,
     # infinicore.float64,
 ]
